2c88fd92b1ce26397fbb599f6803fe8b06b21c1f
mahesh_cool <mahesh910@gmail.com>
2024-07-17 16:49:53 +0530

    created rps4 python file

--- scope.py
+++ scope.py
@@ -1,11 +1,28 @@
+name = "mahesh" #global variable for name
+count = 1  #global variable fro count
+# greeting("John")
 
-
-name = "Dave"
-
-def greeting(name):
+def another():
     color = "blue"
-    print(color)
-    print(name)
+    global count # defining global variable inside the function
+    count += 1
+    # count = 2 local definition inside the function
+    print(count)
+
+    def greeting(name):# definition for function
+        nonlocal color # defining local colour for function
+        color = "red" #used for greeting function only
+        print(color) # print local function colour
+        print(name)#print global name
+        print(count) # print another function result adding +1
+    # print(firstname)
+    greeting("Dave")
+print(count)
+another()
+
+# def san():
+#      another("mahsh")
+#      print(name)
     
-greeting("John")
-print(color)
+
+# san("mahesh")
